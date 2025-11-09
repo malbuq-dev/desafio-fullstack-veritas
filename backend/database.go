19dc6fe06b5068d4d3d7aa4f0f5ead4a49783866
Michael Albuquerque <michael.albuquerque.dos.santos@hotmail.com>
2025-11-09 10:34:18 -0300

    feat: adding database connection code

---
+++ backend/database.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "log"
+    "os"
+
+    "github.com/joho/godotenv"
+    "gorm.io/driver/postgres"
+    "gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+    err := godotenv.Load()
+	
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    dsn := os.Getenv("SUPABASE_DB_URL")
+
+    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    if err != nil {
+        log.Fatal("Failed to connect to Supabase:", err)
+    }
+
+    DB = db
+    log.Println("Connected to Supabase successfully!")
+}
