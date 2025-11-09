f556acd76e21749b8dacfdbb9ed3213877f18791
Michael Albuquerque <michael.albuquerque.dos.santos@hotmail.com>
2025-11-09 10:22:09 -0300

    feat: adding routes code for the app

---
+++ backend/routes.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func SetupRouter() *gin.Engine {
+    router := gin.Default()
+
+    taskRoutes := router.Group("/tasks")
+    {
+        taskRoutes.POST("/", CreateTask)
+    }
+
+    return router
+}
