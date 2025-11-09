2cc3fa958cf36ee93aa145e9957af3599cdb7b1c
Michael Albuquerque <michael.albuquerque.dos.santos@hotmail.com>
2025-11-09 10:21:52 -0300

    feat: adding start up code for the appication

---
+++ backend/main.go
@@ -0,0 +1,22 @@
+package main
+import (
+    "fmt"
+    "log"
+    "os"
+
+    "github.com/joho/godotenv"
+)
+
+func main() {
+    _ = godotenv.Load()
+
+    port := os.Getenv("PORT")
+
+    if port == "" { //Edge case pra rodar localmente
+        port = "8080"
+    }
+
+    router := SetupRouter()
+    log.Printf("Running on http://localhost:%s", port)
+    router.Run(fmt.Sprintf(":%s", port))
+}
