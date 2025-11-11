8faf9c7cd433ee278a4cbcc92e322290058c2363
Michael Albuquerque <michael.albuquerque.dos.santos@hotmail.com>
2025-11-11 17:01:47 -0300

    feat: adding cors to connect it to frontend

--- backend/routes.go
+++ backend/routes.go
@@ -1,9 +1,32 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func SetupRouter() *gin.Engine {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, skipping...")
+	}
+
 	router := gin.Default()
+
+	config := cors.Config{
+		AllowOrigins: []string{
+			os.Getenv("FRONT_END_URL_DEV"), 
+		},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Accept"},
+		ExposeHeaders: []string{"Content-Length"},
+		AllowCredentials: true,
+	}
+
+	router.Use(cors.New(config))
 
 	taskRoutes := router.Group("/tasks")
 	{
